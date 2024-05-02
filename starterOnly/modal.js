cdcd2353641475e6171ae53ee77a5d48e577d087
zafle <fleutotanne@free.fr>
2024-05-02 09:58:55 +0200

    Fix Issue #1: close modal with animation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelector(".close");
+const modalContent = document.querySelector(".content");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +22,32 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// launch close modal event
+closeModalBtn.addEventListener("click", closeModal);
 
+// close modal form
+function closeModal() {
+
+  const closeModalKeyframes = [
+    {
+      transform: "translateY(0px)",
+      opacity: 1
+    },
+    {
+      transform: "translateY(-150px)",
+      opacity: 0,
+    }
+  ];
+
+  const closeModalTimingOptions = {
+    duration: 400,
+  };
+
+  const closeModalAnimation = modalContent.animate(closeModalKeyframes, closeModalTimingOptions);
+
+  closeModalAnimation.play();
+
+  closeModalAnimation.onfinish = () => {
+    modalbg.style.display = "none";
+  };
+}
