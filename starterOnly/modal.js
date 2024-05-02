420b134e871cef3fdf67ff8e34a8192288b07e22
zafle <fleutotanne@free.fr>
2024-05-02 14:45:57 +0200

    Fix Issue#2 form validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,7 +16,15 @@ const closeModalBtn = document.querySelector(".close");
 const modalContent = document.querySelector(".content");
 
 // form
-const formData = document.querySelectorAll(".formData");
+// const formData = document.querySelectorAll(".formData");
+const reservationForm = document.querySelector("form[name='reserve']");
+const submitBtn = document.querySelector(".btn-submit");
+const firstNameInput = document.getElementById("first");
+const lastNameInput = document.getElementById("last");
+const emailInput = document.getElementById("email");
+const quantityInput = document.getElementById("quantity");
+const locationInputs = document.querySelectorAll("input[name='location']");
+const agreementInput = document.getElementById("checkbox1");
 
 
 // ######## open / close modal #######
@@ -62,3 +70,103 @@ function closeModal() {
     modalbg.style.display = "none";
   };
 }
+
+// ######### Form validation #######
+
+
+
+/**
+ * This function validates the first name / the last name
+ * Conditions: 2 characters minimum / non empty
+ * @param {string} name
+ * @throws {Error}
+ */
+function validateName(name) {
+  if (name.length < 2) {
+    throw new Error("Ce champ doit comporter au moins 2 caractères");
+  }
+}
+
+/**
+ * This function validates the email
+ * @param {string} email
+ * @throws {Error}
+ */
+function validateEmail(email) {
+  let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
+  if (!emailRegExp.test(email)) {
+      throw new Error("L'email n'est pas valide");
+  }
+}
+
+/**
+ * This function validates the quantity of games
+ * @param {number} quantity
+ * @throws {Error}
+ */
+function validateQuantity(quantity) {
+  if ( !( 0 <= quantity ) && !( quantity <= 99 )) {
+    throw new Error("La quantité n'est pas valide");
+  }
+}
+
+/**
+ * This function validates the radio button for location selection
+ * @throws {Error}
+ */
+function validateLocation() {
+  let radioBtnChecked = 0;
+  for (const locationInput of locationInputs) {
+    if (locationInput.checked) {
+      radioBtnChecked++;
+      break;
+    }
+  }
+  if (radioBtnChecked !== 1) {
+    throw new Error("Vous devez sélectionner un tournoi");
+  }
+}
+
+/**
+ * This function validates the agreement checkbox
+ * @throws {Error}
+ */
+function validateAgreement() {
+  if (!agreementInput.checked) {
+    throw new Error("Vous devez accepter les conditions d'utilisation");
+  }
+}
+
+/**
+ * This function validates the form's entries
+ */
+function validateForm() {
+  try {
+    let firstName = firstNameInput.value;
+    validateName(firstName);
+
+    let lastName = lastNameInput.value;
+    validateName(lastName);
+
+    let email = emailInput.value;
+    validateEmail(email);
+
+    let quantity = parseInt(quantityInput.value);
+    validateQuantity(quantity);
+
+    validateLocation();
+
+    validateAgreement();
+
+    reservationForm.submit();
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// launch form validation on submit
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  validateForm();
+});
