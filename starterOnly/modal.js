5f47935fb78425f19a78f0787e75f7cc64e2bf6e
zafle <fleutotanne@free.fr>
2024-05-03 17:55:39 +0200

    Fix Issue#3: error messages displaying when submiting the form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,7 +16,7 @@ const closeModalBtn = document.querySelector(".close");
 const modalContent = document.querySelector(".content");
 
 // form
-// const formData = document.querySelectorAll(".formData");
+const formData = document.querySelectorAll(".formData");
 const reservationForm = document.querySelector("form[name='reserve']");
 const submitBtn = document.querySelector(".btn-submit");
 const firstNameInput = document.getElementById("first");
@@ -74,15 +74,24 @@ function closeModal() {
 
 // ######### Form validation #######
 
-
-
 /**
- * This function validates the first name / the last name
+ * This function validates the first name
  * Conditions: 2 characters minimum / non empty
- * @param {string} name
  * @throws {Error}
  */
-function validateName(name) {
+function validateFirstName() {
+  let name = firstNameInput.value;
+  if (name.length < 2) {
+    throw new Error("Ce champ doit comporter au moins 2 caractères");
+  }
+}
+/**
+ * This function validates the last name
+ * Conditions: 2 characters minimum / non empty
+ * @throws {Error}
+ */
+function validateLastName() {
+  let name = lastNameInput.value;
   if (name.length < 2) {
     throw new Error("Ce champ doit comporter au moins 2 caractères");
   }
@@ -90,33 +99,42 @@ function validateName(name) {
 
 /**
  * This function validates the email
- * @param {string} email
  * @throws {Error}
  */
-function validateEmail(email) {
+function validateEmail() {
+  let email = emailInput.value;
   let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
   if (!emailRegExp.test(email)) {
-      throw new Error("L'email n'est pas valide");
+    throw new Error("L'email n'est pas valide");
   }
 }
 
-function validateDate(date) {
-  let dateOfBirth = new Date(date);
-  let limitDate = new Date();
-  limitDate.setFullYear(limitDate.getFullYear() - 4);
-
-  if ( !(dateOfBirth.toISOString() < limitDate.toISOString()) ) {
+/**
+ * This function validates the birthdate
+ * Condition : 4 years old minimum
+ * @throws {Error}
+ */
+function validateDate() {
+  let date = dateInput.value;
+  if (date !== "") {
+    let dateOfBirth = new Date(date);
+    let limitDate = new Date();
+    limitDate.setFullYear(limitDate.getFullYear() - 4);
+    if ( !(dateOfBirth.toISOString() < limitDate.toISOString()) ) {
+      throw new Error("Vous devez indiquer une date de naissance valide");
+    }
+  } else {
     throw new Error("Vous devez indiquer une date de naissance valide");
   }
 }
 
 /**
  * This function validates the quantity of games
- * @param {number} quantity
  * @throws {Error}
  */
-function validateQuantity(quantity) {
-  if ( !( 0 <= quantity ) && !( quantity <= 99 )) {
+function validateQuantity() {
+  let quantity = quantityInput.value;
+  if (quantity <= 0 || quantity > 99 || quantity === "") {
     throw new Error("La quantité n'est pas valide");
   }
 }
@@ -149,35 +167,111 @@ function validateAgreement() {
 }
 
 /**
- * This function validates the form's entries
+ * This function displays an error message
+ * @param {HTMLElement} input
+ * @param {string} error
  */
-function validateForm() {
-  try {
-    let firstName = firstNameInput.value;
-    validateName(firstName);
+function displayErrorMessage(input, error) {
+  const inputParent = input.closest(".formData");
+  inputParent.dataset.error = error.message;
+  inputParent.dataset.errorVisible = true;
+}
 
-    let lastName = lastNameInput.value;
-    validateName(lastName);
-
-    let email = emailInput.value;
-    validateEmail(email);
-
-    let date = dateInput.value;
-    validateDate(date);
-
-    let quantity = parseInt(quantityInput.value);
-    validateQuantity(quantity);
-
-    validateLocation();
-
-    validateAgreement();
-
-    reservationForm.submit();
-
-  } catch (error) {
-    console.log(error);
+/**
+ * This function reset error messages
+ * @param {HTMLElement} input
+ */
+function resetErrorMessage(input) {
+  const item = input.closest(".formData");
+  if (item.dataset.error && item.dataset.errorVisible) {
+    delete item.dataset.error;
+    delete item.dataset.errorVisible;
   }
 }
+
+/**
+ * This function erases the previous errors messages,
+ * call the validation function for each input (validate parameter)
+ * and displays the error message if validation failed
+ * @param {HTMLElement} input /to target the good HTML element when erase / display error message
+ * @param {function} validate /function to validate the input
+ * @param {number} errors /count the number of errors during the submit validation process
+ * @return {number}
+ */
+function validateInput(input, validate, errors) {
+  resetErrorMessage(input);
+  try {
+    validate();
+    return errors;
+  } catch (error) {
+    displayErrorMessage(input, error);
+    errors++;
+    return errors;
+  }
+}
+
+/**
+ * This function validates the form's entries and sends the form if no errors
+ */
+function validateForm() {
+
+  let errors = 0;
+
+  // delete all error messages
+  for (const item of formData) {
+    if (item.dataset.error && item.dataset.errorVisible) {
+      delete item.dataset.error;
+      delete item.dataset.errorVisible;
+    }
+  }
+
+  errors = validateInput(firstNameInput, validateFirstName, errors);
+  errors = validateInput(lastNameInput, validateLastName, errors);
+  errors = validateInput(emailInput, validateEmail, errors);
+  errors = validateInput(dateInput, validateDate, errors);
+  errors = validateInput(quantityInput, validateQuantity, errors);
+  errors = validateInput(locationInputs[0], validateLocation, errors);
+  errors = validateInput(agreementInput, validateAgreement, errors);
+
+  console.log("totalerrors : " + errors);
+
+  // if no errors, send the form
+  if (errors === 0) {
+    reservationForm.submit();
+  }
+}
+
+// launch validation for each input on change event
+firstNameInput.addEventListener("change", (event) => {
+  event.preventDefault();
+  validateInput(firstNameInput, validateFirstName);
+});
+lastNameInput.addEventListener("change", (event) => {
+  event.preventDefault();
+  validateInput(lastNameInput, validateLastName);
+});
+emailInput.addEventListener("change", (event) => {
+  event.preventDefault();
+  validateInput(emailInput, validateEmail);
+});
+dateInput.addEventListener("change", (event) => {
+  event.preventDefault();
+  validateInput(dateInput, validateDate);
+});
+quantityInput.addEventListener("change", (event) => {
+  event.preventDefault();
+  validateInput(quantityInput, validateQuantity);
+});
+locationInputs.forEach( (input) => {
+  input.addEventListener("change", (event) => {
+    event.preventDefault();
+    validateInput(input, validateLocation);
+  });
+});
+agreementInput.addEventListener("change", (event) => {
+  event.preventDefault();
+  validateInput(agreementInput, validateAgreement);
+});
 
 // launch form validation on submit
 submitBtn.addEventListener("click", (event) => {
